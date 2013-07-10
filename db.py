940efd7b059a86fdb3ac30a3215d38e573c72f04
Simon Marchi <simon.marchi@polymtl.ca>
2013-07-09 21:15:28 -0400

    Revert MongoClient to Connection for older versions

--- db.py
+++ db.py
@@ -1,9 +1,8 @@
-from pymongo import MongoClient
+from pymongo import Connection
 
 class DBConnection():
   def __init__(self, dbname='biciklo'):
-    self.client = MongoClient()
+    self.client = Connection()
     self.db = self.client[dbname]
     self.membres = self.db.membres
     self.pieces = self.db.pieces
-
