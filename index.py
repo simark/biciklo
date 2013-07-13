8ca8ba8779d216e7121d6754c5b05f2b1f7c2948
Simon Marchi <simon.marchi@polymtl.ca>
2013-07-13 02:11:51 -0400

    Ajout de transform dans ParseIncoming

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 from datetime import datetime
 import json
 import os
 import time
 import httplib
+import traceback
 
 import pymongo
 from bson import json_util
@@ -42,69 +44,129 @@ class RequestError(Exception):
   def __str__(self):
     return "%s (%d)" % (self.msg, self.status)
 
+api_boolean = {'oui': True, 'non': False}
+
+"""
+Méthode déclarative pour décrire les arguments nécessaires pour la
+création/modification de chaque type de ressource de l'API.
+
+Ce tableau est utilisé principalement par ParseIncoming().
+
+Pour chaque clé (p.e. 'membres'), on a:
+  * required: liste de clés nécessaires lors de la création (POST)
+  * optional: liste de clés optionnelles
+  * valid: dict avec les noms de paramètres comme clés.
+           Si la valeur est une liste, la valeur fournie par le client
+           doit se trouver dans cette liste pour être valide. S'il
+           s'agit d'un objet "callable", celui-ci est appelé et doit
+           retourner True ou False pour indiquer si la valeur est
+           valide. Si la valeur est un dict, la valeur fournie par le
+           client doit se trouver dans la liste de clés du dict pour
+           être valide.
+  * transform: dict avec les noms de paramètres comme clés.
+               Si la valeur est un dict, celui-ci est utilisé pour faire
+               la transformation (le paramètre est utilisé comme clé
+               pour déduire la valeur transformée). Si la valeur est un
+               objet "callable", celui-ci est appelé et doit retourner
+               la valeur transformée.
+
+Maybe TODO: peut-être qu'un seul callback qui fait validation + transform ce serait assez...
+"""
 validation = {
   'membres': {
-    'required': ['prenom', 'nom'],
-    'optional': ['courriel', 'listedenvoi', 'provenance'],
+    'req': ['prenom', 'nom'],
+    'opt': ['courriel', 'listedenvoi', 'provenance', 'estbenevole', 'telephone'],
     'valid': {
-      'listedenvoi': ['non', 'oui', 'fait']
+      'listedenvoi': ['non', 'oui', 'fait'],
+      'estbenevole': api_boolean
+    },
+    'transform': {
+      'estbenevole': api_boolean
     }
   },
   'pieces': {
-    'required': ['numero'],
-    'optional': ['section', 'nom', 'reference', 'caracteristique', 'numerobabac', 'prixbabac', 'quantiteneuf', 'prixneuf', 'quantiteusage', 'prixusage', 'remarques'],
+    'req': ['numero'],
+    'opt': ['section', 'nom', 'reference', 'caracteristique', 'numerobabac', 'prixbabac', 'quantiteneuf', 'prixneuf', 'quantiteusage', 'prixusage', 'remarques'],
     'valid': {
-      'numero': unicode.isdigit
+      'numero': unicode.isdigit,
+      'numerobabac': unicode.isdigit,
+      'prixneuf': unicode.isdigit,
+      'prixusage': unicode.isdigit,
+      'prixbabac': unicode.isdigit,
+      'quantiteneuf': unicode.isdigit,
+      'quantiteusage': unicode.isdigit,
+    },
+    'transform': {
+      'numero': int,
+      'numerobabac': int,
+      'prixneuf': int,
+      'prixusage': int,
+      'prixbabac': int,
+      'quantiteneuf': int,
+      'quantiteusage': int,
     }
   },
   'factures': {
-    'required': ['membre'],
-    'optional': ['benevole'],
+    'req': ['membre'],
+    'opt': ['benevole', 'fermee'],
     'valid': {
       'membre': unicode.isdigit,
-      'benevole': unicode.isdigit
+      'benevole': unicode.isdigit,
+      'fermee': api_boolean,
+    },
+    'transform': {
+      'membre': int,
+      'benevole': int,
+      'fermee': api_boolean,
     }
-  }
+  },
 }
 
 """
   Throws RequestError if missing parameter/wrong value.
 """
 def ParseIncoming(data, collection_name, throw_if_required_missing = True):
-  def ValidateValue(valid_values, key, value):
-    if key in valid_values:
-      valid = valid_values[key]
-      if hasattr(valid, '__call__'):
-        if not valid(value):
+  def ValidateValue(valid, key, value):
+    if key in valid:
+      validate = valid[key]
+      if hasattr(validate, '__call__'):
+        if not validate(value):
           raise RequestError(httplib.BAD_REQUEST, "Valeur invalide pour %s" % key)
-      elif isinstance(valid, list):
-        if not value in valid:
+      elif isinstance(validate, list) or isinstance(validate, dict):
+        if not value in validate:
           raise RequestError(httplib.BAD_REQUEST, "Valeur invalide pour %s" % key)
 
-  if collection_name in validation:
-    required_keys = validation[collection_name]['required']
-    optional_keys = validation[collection_name]['optional']
-    valid_values  = validation[collection_name]['valid']
-  else:
-    required_keys = {}
-    optional_keys = {}
-    valid_values = {}
+  def TransformValue(transformations, key, value):
+    if key in transformations:
+      transformation = transformations[key]
+      if hasattr(transformation, '__call__'):
+        return transformation(value)
+      elif isinstance(transformation, dict):
+        return transformation[value]
+
+    return value
+
+  v = validation[collection_name] if collection_name in validation else {}
+  required_keys = v['req'] if 'req' in v else {}
+  optional_keys = v['opt'] if 'opt' in v else {}
+  valid = v['valid'] if 'valid' in v else {}
+  transform = v['transform'] if 'transform' in v else {}
 
   ret = {}
 
   for key in required_keys:
-    if key not in data and throw_if_required_missing:
+    if key in data:
+      value = data[key]
+      ValidateValue(valid, key, value)
+      ret[key] = TransformValue(transform, key, value)
+    elif throw_if_required_missing:
       raise RequestError(httplib.BAD_REQUEST, "Parametre manquant: %s" % key)
-
-    value = data[key]
-    ValidateValue(valid_values, key, value)
-    ret[key] = value
 
   for key in optional_keys:
     if key in data:
       value = data[key]
-      ValidateValue(valid_values, key, value)
-      ret[key] = value
+      ValidateValue(valid, key, value)
+      ret[key] = TransformValue(transform, key, value)
 
   return ret
 
@@ -113,10 +175,24 @@ def ParseIncoming(data, collection_name, throw_if_required_missing = True):
 def Drop():
   db.DBConnection().membres.remove()
   db.DBConnection().pieces.remove()
+  db.DBConnection().facture.remove()
 
   return "ok"
 
-# api membres
+# api
+
+"""
+Les handlers sont généralement organisés comme ceci (certaines étapes
+sont parfois omises):
+1- Vérifier si la resource existe vraiment, sinon lancer un 404.
+2- Décoder les paramètres fournis par le client et lancer un 400 si des
+   paramètres sont manquants ou invalides (fait par ParseIncoming).
+3- Ajouter des données supplémentaires au besoin, comme le datetime
+   actuel, par exemple.
+4- Faire une vérifications "sémantique". Par exemple, vérifier que pour
+   un champ qui représente un # de membre, ce membre existe bel et bien.
+5- Faire l'opération demandée (GET, POST, PUT ou DELETE)
+"""
 @app.route('/api/membres', methods=['GET'])
 def GetMembres():
   result = {}
@@ -166,11 +242,11 @@ def PutMembres(numero):
   status = 200
 
   try:
-    valeurs = ParseIncoming(request.form, 'membres', False)
+    membre = ParseIncoming(request.form, 'membres', False)
 
     update_result = db.DBConnection().membres.update(
         {'numero': numero},
-        {'$set': valeurs},
+        {'$set': membre},
         safe = True
     )
 
@@ -184,6 +260,7 @@ def PutMembres(numero):
   except Exception as ex:
     status = 500
     result = str(ex)
+    traceback.print_exc()
 
   return jsonify(result), status
 
@@ -217,7 +294,6 @@ def GetMembresNumero(numero):
 
   return jsonify(result), status
 
-
 # api pieces
 @app.route('/api/pieces', methods=['GET'])
 def GetPieces():
@@ -242,8 +318,6 @@ def PostPieces():
   try:
     piece = ParseIncoming(request.form, 'pieces')
 
-    piece['numero'] = int(piece['numero'])
-
     if PieceExiste(piece['numero']):
       raise RequestError(httplib.CONFLICT, 'Ce numero de piece est deja pris')
 
@@ -266,17 +340,18 @@ def PutPieces(numero):
   status = httplib.NO_CONTENT
 
   try:
-    valeurs = ParseIncoming(request.form, 'pieces', False)
+    if not PieceExiste(numero):
+      raise RequestError(httplib.NOT_FOUND, "Cette pièce n'existe pas.")
+
+    piece = ParseIncoming(request.form, 'pieces', False)
+
+    if PieceExiste(piece['numero']) and numero != piece['numero']:
+      raise RequestError(httplib.CONFLICT, 'Ce numero de piece est deja pris')
 
     update_result = db.DBConnection().pieces.update(
         {'numero': numero},
-        {'$set': valeurs},
-        safe = True
+        {'$set': piece}
     )
-
-    if not update_result['updatedExisting']:
-      status = 404
-      result = str('Piece inexistante')
 
   except RequestError as ex:
     status = ex.status
@@ -337,7 +412,7 @@ def ValidationFactures(facture):
   if 'membre' in facture and not MembreExiste(facture['membre']):
     raise RequestError(httplib.UNPROCESSABLE_ENTITY, "Ce membre n'existe pas.")
 
-  if 'benevole' in facture and not BenevoleExiste(facture['benevole']):
+  if 'benevole' in facture and not EstBenevole(facture['benevole']):
     raise RequestError(httplib.UNPROCESSABLE_ENTITY, "Ce benevole n'existe pas.")
 
 @app.route('/api/factures', methods=['POST'])
@@ -353,9 +428,6 @@ def PostFactures():
 
     facture['numero'] = ObtenirProchainNumeroDeFacture()
     facture['date'] = datetime.now()
-    facture['membre'] = int(facture['membre'])
-    if 'benevole' in facture:
-      facture['benevole'] = int(facture['benevole'])
 
     ValidationFactures(facture)
 
@@ -441,11 +513,9 @@ def MembreExiste(numero):
 def PieceExiste(numero):
   return db.DBConnection().pieces.find_one({'numero': numero}) != None
 
-def BenevoleExiste(numero):
-  # En attendant...
-  #return db.DBConnection().benevole.find_one({'numero': numero}) != None
-  return True
-
+def EstBenevole(numero):
+  membre = db.DBConnection().membres.find_one({'numero': numero, 'estbenevole': True})
+  return membre != None
 
 # l'app web
 @app.route('/membres/', methods=['GET'])
