e93cdf65dfeb82406318966b6ef81d230e12e689
Simon Marchi <simon.marchi@polymtl.ca>
2013-07-31 00:02:28 -0400

    Améliorations page factures fermées
    Entre autres, fixes #8.

--- static/js/factures-fermees.js
+++ static/js/factures-fermees.js
@@ -13,11 +13,15 @@ function InitTableauFacturesFermees() {
        sInfoFiltered: "(filtré de _MAX_ entrées en tout)",
        sSearch: "rechercher&nbsp;:",
      },
-     'fnRowCallback': function( nRow, aData, iDisplayIndex ) {
-       // Appliquer NombreVersPrix sur la colonne du montant total
+     'fnCreatedRow': function( nRow, aData, iDisplayIndex ) {
        var colonnePrix = 3;
+       var colonneReouvrir = 4;
 
+       // Appliquer NombreVersPrix sur la colonne du montant total
        $('td:eq(' + colonnePrix + ')', nRow).html( NombreVersPrix(aData[colonnePrix]) );
+
+       // Mettre le handler du clic sur le bouton pour réouvrir les factures
+       $(nRow).find('.reouvrir-facture').click(ReouvrirFacture);
 
        return nRow;
 		},
@@ -26,14 +30,14 @@ function InitTableauFacturesFermees() {
 
 function ReouvrirFacture() {
   var tr = $(this).closest('tr');
-  numeroFacture = tr.attr('data-numero-facture');
+  var numeroFacture = tr.attr('data-numero-facture');
   var data = {'complete': 'non'};
   $.ajax({
       'url': '/api/factures/' + numeroFacture,
       'type': 'PUT',
       'data': data,
     }).done(function (data, textStatus, jqXHR) {
-      AfficherSucces('Facture réouverte');
+      AfficherSucces('Facture #' + numeroFacture + ' réouverte');
       $('#factures-fermees').dataTable().fnDeleteRow(tr.get(0));
     }).fail(DisplayError);
 }
@@ -41,6 +45,4 @@ function ReouvrirFacture() {
 
 $(document).ready(function() {
   InitTableauFacturesFermees();
-
-  $('.reouvrir-facture').click(ReouvrirFacture);
 });
