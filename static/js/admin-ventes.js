53f2d3b7e5b8eb71caee9accefef63b4d883c911
Simon Marchi <simon.marchi@polymtl.ca>
2014-03-29 22:05:09 -0400

    More information in ventes page
    Signed-off-by: Simon Marchi <simon.marchi@polymtl.ca>

--- static/js/admin-ventes.js
+++ static/js/admin-ventes.js
@@ -1,19 +1,12 @@
+pieces = null;
+
 $(document).ready(function() {
 	$('#debut').datepicker({language: 'fr'});
 	$('#fin').datepicker({language: 'fr'});
 
 	$('#ventes').dataTable({
-				'aaData': [],
 				'bPaginate': false,
-				'aoColumns': [
-						{
-							sTitle: '#',
-						},
-						{
-							sTitle: 'quantité',
-						},
-				],
-			});
+});
 
 	$('#afficher').click(function() {
 		var debut = $('#debut').val();
@@ -34,35 +27,60 @@ $(document).ready(function() {
 
 		url += params.join("&");
 
-		$.get(url).done(function (data, textStatus, jqXHR) {
-			var rows = [];
-			var quantites = {};
-
-			// Calculer la quantité par pièce
-			for (i in data) {
-				var facture = data[i];
-				for (j in facture.pieces) {
-					var achat = facture.pieces[j];
-
-					if (!(achat.numero in quantites)) {
-						quantites[achat.numero] = 0;
-					}
-
-					quantites[achat.numero] += achat.quantite;
+		/* La première fois qu'on va chercher des ventes, on va d'abord chercher la liste de pièces. */
+		if (pieces == null) {
+			$.get("/api/pieces").done(function (data, textStatus, jqXHR) {
+				liste_pieces = data;
+				pieces = {};
+				for (k in liste_pieces) {
+					piece = liste_pieces[k];
+					pieces[piece.numero] = piece;
 				}
-			}
 
-			// Générer les lignes du tableau
-			for (numero in quantites) {
-				var row = [numero, quantites[numero]];
-				rows.push(row);
-			}
+				$.get(url).done(RemplirTableauVentes).fail(DisplayError);
+			}).fail(DisplayError);
+		} else {
+			$.get(url).done(RemplirTableauVentes).fail(DisplayError);
+		}
 
-			$('#ventes').dataTable().fnClearTable();
-			$('#ventes').dataTable().fnAddData(rows);
 
-		}).fail(DisplayError);
 
 		return false;
 	});
 });
+
+function RemplirTableauVentes(data, textStatus, jqXHR) {
+	var rows = [];
+	var quantites = {};
+
+	// Calculer la quantité par pièce
+	for (i in data) {
+		var facture = data[i];
+		for (j in facture.pieces) {
+			var achat = facture.pieces[j];
+
+			if (!(achat.numero in quantites)) {
+				quantites[achat.numero] = 0;
+			}
+
+			quantites[achat.numero] += achat.quantite;
+		}
+	}
+
+	// Générer les lignes du tableau
+	for (numero in quantites) {
+		var piece = pieces[numero];
+		var numerobabac = 'numerobabac' in piece ? piece.numerobabac : "";
+		var section = piece.section;
+		var nom = piece.nom;
+		var reference = 'reference' in piece ? piece.reference : "";
+		var caracteristique = piece.caracteristique;
+
+		var row = [numero, numerobabac, section, nom, reference, caracteristique, Math.round(quantites[numero])];
+		rows.push(row);
+	}
+
+	$('#ventes').dataTable().fnClearTable();
+	$('#ventes').dataTable().fnAddData(rows);
+
+}
