f9079da60429c0957965af7c13e4ce3730b8a686
Simon Marchi <simon.marchi@polymtl.ca>
2015-04-17 17:04:30 -0400

    db: pymongo Connection -> MongoClient

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
-from pymongo import Connection
+import pymongo
 
 class DBConnection():
   def __init__(self, dbname='biciklo'):
-    self.client = Connection()
+    self.client = pymongo.MongoClient()
     self.db = self.client[dbname]
     self.membres = self.db.membres
     self.pieces = self.db.pieces
