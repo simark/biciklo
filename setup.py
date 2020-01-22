5bb36020836e12d201aed60171ea58aab3b70bef
Normand Cyr <norm@normandcyr.com>
2020-01-21 23:57:10 -0500

    utiliser version 0.1.0 de recherche_babac2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         'bs4',
         'pyyaml',
         'lxml',
-        'recherche_babac2==0.0.5',
+        'recherche_babac2==0.1.0',
     ],
     zip_safe=False,
     include_package_data=True,
