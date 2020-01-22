ae2a121eed273cb5788a43eb439c9379e32b77f6
Normand Cyr <norm@normandcyr.com>
2020-01-21 23:21:34 -0500

    get rid of the default secret_key

--- biciklo/biciklo.py
+++ biciklo/biciklo.py
@@ -31,7 +31,7 @@ from recherche_babac2 import recherche_babac2 as rb2
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+app.config['SECRET_KEY'] = os.urandom(24)
 
 # numéro de pièce des abonnements et durées
 abonnements = {
